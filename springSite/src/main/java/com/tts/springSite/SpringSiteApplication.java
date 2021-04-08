6da54634de9445c56736062f505dd0dc0f658200
mteek <75220853+mteek@users.noreply.github.com>
2021-04-07 23:35:18 -0400

    Add files via upload
    mt springsite

---
+++ springSite/src/main/java/com/tts/springSite/SpringSiteApplication.java
@@ -0,0 +1,14 @@
+package com.tts.springSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSiteApplication{
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSiteApplication.class, args);
+	}
+}
+
